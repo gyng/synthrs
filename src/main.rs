d33795b5daad1577542092da9e51458127b1dd22
Ng Guoyou <fauxface+github@gmail.com>
2014-12-12 07:02:18 +0800

    Fix wrong frequency for waves

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 #![feature(lang_items, unboxed_closures)]
 
 use std::io::{File, IoResult, Truncate, Write};
+use std::f64::consts::PI;
 use std::num::Float;
 use std::num::FloatMath;
 use std::rand;
 use std::rand::Rng;
 
-struct SinWave(pub f64);
+struct SineWave(pub f64);
 
-impl Fn<(f64, ), f64> for SinWave {
+impl Fn<(f64, ), f64> for SineWave {
     extern "rust-call" fn call(&self, (t, ): (f64, )) -> f64 {
-        let SinWave(frequency) = *self;
-        FloatMath::sin(t * frequency)
+        let SineWave(frequency) = *self;
+        FloatMath::sin(t * frequency * 2.0 * PI)
     }
 }
 
@@ -20,7 +21,7 @@ struct SquareWave(pub f64);
 impl Fn<(f64, ), f64> for SquareWave {
     extern "rust-call" fn call(&self, (t, ): (f64, )) -> f64 {
         let SquareWave(frequency) = *self;
-        let sin_wave = SinWave(frequency);
+        let sin_wave = SineWave(frequency);
         if sin_wave(t).is_positive() { 1.0 } else { -1.0 }
     }
 }
@@ -116,10 +117,10 @@ fn write_wav(filename: &str, sample_rate: uint, samples: Vec<i16>) -> IoResult<(
 fn main() {
     println!("Hello, synthrs!");
 
-    write_pcm("out/sin.pcm", make_sample_16(1.0, 44100, SinWave(1000.0))).ok();
-    write_wav("out/sin.wav", 44100, make_sample_16(1.0, 44100, SinWave(1000.0))).ok();
-    write_wav("out/square.wav", 44100, make_sample_16(1.0, 44100, SquareWave(1000.0))).ok();
-    write_wav("out/sawtooth.wav", 44100, make_sample_16(1.0, 44100, SawtoothWave(1000.0))).ok();
+    write_pcm("out/sin.pcm", make_sample_16(1.0, 44100, SineWave(440.0))).ok();
+    write_wav("out/sin.wav", 44100, make_sample_16(1.0, 44100, SineWave(440.0))).ok();
+    write_wav("out/square.wav", 44100, make_sample_16(1.0, 44100, SquareWave(440.0))).ok();
+    write_wav("out/sawtooth.wav", 44100, make_sample_16(1.0, 44100, SawtoothWave(440.0))).ok();
 
     write_wav("out/wolftone.wav", 44100, make_sample_16(1.0, 44100, |t: f64| -> f64 {
         (SquareWave(1000.0)(t) + SquareWave(1020.0)(t)) / 2.0
@@ -135,18 +136,31 @@ fn main() {
         let max_t = 1.0; // Duration of clip in seconds
         let range = max_f - min_f;
         let f = max_f - (max_t - t) * range;
-        SinWave(f)(t)
+        SineWave(f)(t)
     })).ok();
 
     write_wav("out/racecar.wav", 44100, make_sample_16(15.0, 44100, |t: f64| -> f64 {
         let mut rng = rand::task_rng();
         let mut out = 0.0;
-        if t < 14.0 { out += SawtoothWave(40.63 * (t / 2.0))(t); } // Engine
-        if t < 1.0 { out += SawtoothWave(30.0)(t) * 10.0; } // Engine start
-        if t < 14.0 && t.tan() > 0.0 { out += SquareWave(t.fract() * 523.25)(t); } // Gear
-        if t < 14.0 && t.tan() < 0.0 { out += SquareWave(t.sin() * 1046.5)(t); } // Gear
-        if t < 14.0 && t.tan() > 0.866 { out += SquareWave(t.fract() * 2093.0)(t); } // Gear
+        if t < 14.0 { out += SawtoothWave(20.315 * (t / 2.0))(t); } // Engine
+        if t < 1.0 { out += SawtoothWave(15.0)(t) * 10.0; } // Engine start
+        if t < 14.0 && t.tan() > 0.0 { out += SquareWave(t.fract() * 261.625)(t); } // Gear
+        if t < 14.0 && t.tan() < 0.0 { out += SquareWave(t.sin() * 523.25)(t); } // Gear
+        if t < 14.0 && t.tan() > 0.866 { out += SquareWave(t.fract() * 1046.5)(t); } // Gear
         if t > 14.0 { out += (rng.gen::<f64>() - 0.5) * 8.0 } // Tree
         (out / 4.0).min(1.0)
+    })).ok();
+
+    write_wav("out/shepard.wav", 44100, make_sample_16(30.0, 44100, |t: f64| -> f64 {
+        let length = 10.0;
+        let t_mod = t % length;
+        let progress = t_mod / length;
+        let tone_a2 = SineWave(110.0 / (1.0 + progress));
+        let tone_a3 = SineWave(220.0 / (1.0 + progress));
+        let tone_a4 = SineWave(440.0 / (1.0 + progress));
+        let tone_a5 = SineWave(880.0 / (1.0 + progress));
+        (tone_a2(t_mod) * (1.0 - progress)
+        + tone_a5(t_mod) * progress
+        + tone_a3(t_mod) + tone_a4(t_mod)) / 4.0
     })).ok();
 }
