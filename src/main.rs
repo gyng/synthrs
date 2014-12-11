4cebf0308b9993c4cedbe877089fa348a46fc8a5
Ng Guoyou <fauxface+github@gmail.com>
2014-12-12 00:26:52 +0800

    Sinwave generation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+#![feature(lang_items, unboxed_closures)]
+
+use std::io::{File, Truncate, Write};
+use std::mem;
+use std::num::Float;
+use std::num::FloatMath;
+
+struct SinWave(pub f64);
+
+impl Fn<(f64, ), f64> for SinWave {
+    extern "rust-call" fn call(&self, (x, ): (f64, )) -> f64 {
+        let SinWave(period) = *self;
+        FloatMath::sin(x * period)
+    }
+}
+
 fn main() {
     println!("Hello, synthrs!");
+
+    let mut bytes: [u8, ..44100] = unsafe { mem::uninitialized() };
+    for i in range(0u, 44100) {
+        bytes[i] = quantize_8bit(generate(i as f64, SinWave(1000.0)))
+        // bytes[i] = generate(i as f64, |x: f64| {
+        //    FloatMath::sin(x / 8000.0)
+        // });
+    }
+
+    write_pcm("test.pcm", &bytes);
+}
+
+fn generate<F>(x: f64, f: F) -> f64 where F: Fn<(f64, ), f64> {
+    f(x)
+}
+
+fn quantize_8bit(y: f64) -> u8 {
+    let step = 2.0 / 8.0; // Max range [-1, 1], 8 bits
+    (((y + 1.0) / step) * 8.0).floor() as u8 // Convert to [0, 7]
+}
+
+fn write_pcm(filename: &str, bytes: &[u8]) {
+    let path = Path::new(filename);
+    let mut f = match File::open_mode(&path, Truncate, Write) {
+        Ok(f) => f,
+        Err(e) => panic!("File error: {}", e)
+    };
+
+    f.write(bytes);
 }
