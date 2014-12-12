48389c3f42ad7f5415696457ca214e15488211fe
Ng Guoyou <fauxface+github@gmail.com>
2014-12-12 10:43:04 +0800

    Remove extra semicolons

--- src/main.rs
+++ src/main.rs
@@ -183,19 +183,19 @@ fn write_wav(filename: &str, sample_rate: uint, samples: Vec<i16>) -> IoResult<(
 fn main() {
     println!("Hello, synthrs!");
 
-    write_pcm("out/sin.pcm", make_sample_16(1.0, 44100, SineWave(440.0))).ok().expect("Failed");;
-    write_wav("out/sin.wav", 44100, make_sample_16(1.0, 44100, SineWave(440.0))).ok().expect("Failed");;
-    write_wav("out/square.wav", 44100, make_sample_16(1.0, 44100, SquareWave(440.0))).ok().expect("Failed");;
-    write_wav("out/sawtooth.wav", 44100, make_sample_16(1.0, 44100, SawtoothWave(440.0))).ok().expect("Failed");;
+    write_pcm("out/sin.pcm", make_sample_16(1.0, 44100, SineWave(440.0))).ok().expect("Failed");
+    write_wav("out/sin.wav", 44100, make_sample_16(1.0, 44100, SineWave(440.0))).ok().expect("Failed");
+    write_wav("out/square.wav", 44100, make_sample_16(1.0, 44100, SquareWave(440.0))).ok().expect("Failed");
+    write_wav("out/sawtooth.wav", 44100, make_sample_16(1.0, 44100, SawtoothWave(440.0))).ok().expect("Failed");
 
     write_wav("out/wolftone.wav", 44100, make_sample_16(1.0, 44100, |t: f64| -> f64 {
         (SquareWave(1000.0)(t) + SquareWave(1020.0)(t)) / 2.0
-    })).ok().expect("Failed");;
+    })).ok().expect("Failed");
 
     write_wav("out/whitenoise.wav", 44100, make_sample_16(1.0, 44100, |_t: f64| -> f64 {
         let mut rng = rand::task_rng();
         (rng.gen::<f64>() - 0.5) * 2.0
-    })).ok().expect("Failed");;
+    })).ok().expect("Failed");
 
     write_wav("out/rising.wav", 44100, make_sample_16(1.0, 44100, |t: f64| -> f64 {
         let (min_f, max_f) = (1000.0, 8000.0);
@@ -203,7 +203,7 @@ fn main() {
         let range = max_f - min_f;
         let f = max_f - (max_t - t) * range;
         SineWave(f)(t)
-    })).ok().expect("Failed");;
+    })).ok().expect("Failed");
 
     write_wav("out/racecar.wav", 44100, make_sample_16(15.0, 44100, |t: f64| -> f64 {
         let mut rng = rand::task_rng();
@@ -215,7 +215,7 @@ fn main() {
         if t < 14.0 && t.tan() > 0.866 { out += SquareWave(t.fract() * 523.25)(t); } // Gear
         if t > 14.0 { out += (rng.gen::<f64>() - 0.5) * 8.0 } // Tree
         (out / 4.0).min(1.0)
-    })).ok().expect("Failed");;
+    })).ok().expect("Failed");
 
     write_wav("out/shepard.wav", 44100, make_sample_16(30.0, 44100, |t: f64| -> f64 {
         let length = 10.0;
@@ -228,7 +228,7 @@ fn main() {
         (tone_a2(t_mod) * (1.0 - progress)
         + tone_a5(t_mod) * progress
         + tone_a3(t_mod) + tone_a4(t_mod)) / 4.0
-    })).ok().expect("Failed");;
+    })).ok().expect("Failed");
 
     // Telecomms
     write_wav("out/dialtone.wav", 44100, make_sample_16(15.0, 44100, |t: f64| -> f64 {
