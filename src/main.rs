551f293b791590468a8c672470b267da0df1053c
Ng Guoyou <fauxface+github@gmail.com>
2014-12-12 21:23:03 +0800

    Get frequency from note
    The function signature needs to be easier to use

--- src/main.rs
+++ src/main.rs
@@ -168,6 +168,13 @@ fn make_sample<F>(length: f64, sample_rate: uint, waveform: F) -> Vec<f64> where
     samples
 }
 
+/// Equal-tempered
+/// 0=C 1=C# 2=D 3=D# 4=E 5=F 6=F# 7=G 8=G# 9=A 10=B
+fn note(a4: f64, note: uint, harmonic: uint) -> f64 {
+    let semitones_from_a4 = harmonic as int * 12 + note as int - 9 - 48;
+    a4 * (semitones_from_a4 as f64 * 2.0.ln() / 12.0).exp()
+}
+
 fn write_pcm(filename: &str, samples: Vec<i16>) -> IoResult<()> {
     let path = Path::new(filename);
     let mut f = match File::open_mode(&path, Truncate, Write) {
@@ -351,4 +358,17 @@ fn it_convolves() {
     let input = vec!(0.0, 0.0, 3.0, 0.0, 3.0, 0.0, 0.0);
     let output = vec!(0.0, 3.0, 3.0, 6.0, 3.0, 3.0, 0.0);
     assert_eq!(convolve(filter, input), output);
+}
+
+#[test]
+fn it_equal_tempers() {
+    let threshold = 0.1;
+    let c4 = 261.63;
+    let a4 = 440.0;
+    let d3 = 146.83;
+    let fs6 = 1479.98;
+    assert!(FloatMath::abs_sub(note(a4, 9, 4), a4) < threshold);
+    assert!(FloatMath::abs_sub(note(a4, 0, 4), c4) < threshold);
+    assert!(FloatMath::abs_sub(note(a4, 2, 3), d3) < threshold);
+    assert!(FloatMath::abs_sub(note(a4, 6, 6), fs6) < threshold);
 }
