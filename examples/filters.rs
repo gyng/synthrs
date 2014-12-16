fdd06838fc4aceac716829c51926d4067e3a151a
Ng Guoyou <fauxface+github@gmail.com>
2014-12-17 05:30:10 +0800

    Fix Rust breaking changes

--- examples/filters.rs
+++ examples/filters.rs
@@ -19,12 +19,12 @@ fn main() {
 
     let lowpass = lowpass_filter(cutoff_from_frequency(400.0, 44100), 0.01);
     write_wav("out/lowpass.wav", 44100,
-        quantize_samples::<i16>(sample.clone()) + quantize_samples::<i16>(convolve(lowpass.clone(), sample.clone()))
+        quantize_samples::<i16>(sample.clone()) + &*quantize_samples::<i16>(convolve(lowpass, sample.clone()))
     ).ok().expect("failed");
 
     let highpass = highpass_filter(cutoff_from_frequency(2000.0, 44100), 0.01);
     write_wav("out/highpass.wav", 44100,
-        quantize_samples::<i16>(sample.clone()) + quantize_samples::<i16>(convolve(highpass.clone(), sample.clone()))
+        quantize_samples::<i16>(sample.clone()) + &*quantize_samples::<i16>(convolve(highpass, sample.clone()))
     ).ok().expect("failed");
 
     let bandpass = bandpass_filter(
@@ -33,7 +33,7 @@ fn main() {
         0.01
     );
     write_wav("out/bandpass.wav", 44100,
-        quantize_samples::<i16>(sample.clone()) + quantize_samples::<i16>(convolve(bandpass.clone(), sample.clone()))
+        quantize_samples::<i16>(sample.clone()) + &*quantize_samples::<i16>(convolve(bandpass, sample.clone()))
     ).ok().expect("failed");
 
     let bandreject = bandreject_filter(
@@ -42,6 +42,6 @@ fn main() {
         0.01
     );
     write_wav("out/bandreject.wav", 44100,
-        quantize_samples::<i16>(sample.clone()) + quantize_samples::<i16>(convolve(bandreject.clone(), sample.clone()))
+        quantize_samples::<i16>(sample.clone()) + &*quantize_samples::<i16>(convolve(bandreject, sample.clone()))
     ).ok().expect("failed");
 }
