ab86429941a3dbac39879aabf1912432029b2b11
Ng Guoyou <fauxface+github@gmail.com>
2014-12-20 04:03:04 +0800

    Rust breaking change: something to do with closures

--- src/synthesizer.rs
+++ src/synthesizer.rs
@@ -15,17 +15,17 @@ pub fn quantize_samples<T>(input: Vec<f64>) -> Vec<T> where T: FromPrimitive {
     input.iter().map(|s| { quantize::<T>(*s) }).collect()
 }
 
-pub fn generate<F>(x: f64, f: F) -> f64 where F: Fn<(f64, ), f64> {
-    f(x)
+pub fn generate<F>(x: f64, f: &F) -> f64 where F: Fn<(f64, ), f64> {
+    f.call((x, ))
 }
 
-pub fn make_samples<F>(length: f64, sample_rate: uint, waveform: F) -> Vec<f64> where F: Fn<(f64, ), f64>+Copy {
+pub fn make_samples<F>(length: f64, sample_rate: uint, waveform: F) -> Vec<f64> where F: Fn<(f64, ), f64> {
     let num_samples = (sample_rate as f64 * length).floor() as uint;
     let mut samples: Vec<f64> = Vec::with_capacity(num_samples);
 
     for i in range(0u, num_samples) {
         let t = i as f64 / sample_rate as f64;
-        samples.push(generate(t, waveform));
+        samples.push(generate(t, &waveform));
     }
 
     samples
