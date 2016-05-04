61123af749cf96d9c6d593c2234e24c6ff4ca062
Ng Guoyou <ng.guoyou@gmail.com>
2016-05-04 13:39:21 +0800

    Document quantize behaviour better

--- src/synthesizer.rs
+++ src/synthesizer.rs
@@ -16,7 +16,7 @@
 use std::mem::size_of;
 
 use num::Float;
-use num::traits::FromPrimitive;
+use num::traits::{Bounded, FromPrimitive, Num};
 
 use filter;
 use music;
@@ -24,9 +24,19 @@ use midi;
 use wave;
 
 /// Quantizes a `f64` sample into `T`.
-pub fn quantize<T>(input: f64) -> T where T: FromPrimitive {
+/// Convert from [-1.0f64, 1.0] to take up full quantization range of type `T`.
+/// Behaviour is not guaranteed for `T` where `T` is unsigned.
+///
+/// ```
+/// use synthrs::synthesizer::quantize;
+///
+/// assert_eq!(quantize::<i8>(1.0f64), 127i8);
+/// assert_eq!(quantize::<f32>(0.0f64), 0.0f32);
+/// assert_eq!(quantize::<u8>(1.0f64), 127u8); // half of available quantization space of a u8 (255)
+/// assert_eq!(quantize::<u8>(-1.0f64), 129u8); // unexpected behaviour
+/// ```
+pub fn quantize<T>(input: f64) -> T where T: Num+FromPrimitive+Bounded {
     let quantization_levels = 2.0.powf(size_of::<T>() as f64 * 8.0) - 1.0;
-    // Convert from [-1, 1] to take up full quantization range
     T::from_f64(input * (quantization_levels / 2.0)).expect("failed to quantize to given type")
 }
 
@@ -40,7 +50,7 @@ pub fn quantize<T>(input: f64) -> T where T: FromPrimitive {
 ///
 /// quantize_samples::<i16>(make_samples(1.0, 44100, SineWave(440.0)));
 /// ```
-pub fn quantize_samples<T>(input: Vec<f64>) -> Vec<T> where T: FromPrimitive {
+pub fn quantize_samples<T>(input: Vec<f64>) -> Vec<T> where T: Num+FromPrimitive+Bounded {
     input.iter().map(|s| { quantize::<T>(*s) }).collect()
 }
 
@@ -139,4 +149,30 @@ pub fn make_samples_from_midi(sample_rate: usize, filename: &str) -> Vec<f64> {
     }
 
     peak_normalize(samples)
+}
+
+#[test]
+fn it_peak_normalizes() {
+    let input_negative = vec![-2.0f64, 1.0, -1.0];
+    let output_negative = peak_normalize(input_negative);
+    assert_eq!(output_negative, vec![-1.0f64, 0.5, -0.5]);
+
+    let input_positive = vec![2.0f64, 1.0, -1.0];
+    let output_positive = peak_normalize(input_positive);
+    assert_eq!(output_positive, vec![1.0f64, 0.5, -0.5])
+}
+
+#[cfg(test)]
+use std::i8;
+
+#[cfg(test)]
+use std::i16;
+
+#[test]
+fn it_quantizes() {
+    assert_eq!(i8::MAX, quantize::<i8>(1.0));
+    // assert_eq!(i8::MIN + 1, quantize::<i8>(-1.0)); // Bad quantization behaviour?
+    assert_eq!(i16::MAX, quantize::<i16>(1.0));
+    assert_eq!(0.0f32, quantize::<f32>(0.0));
+    // assert_eq!(u8::MAX, quantize::<u8>(1.0)); // TODO: Make quantization work for unsigned types
 }
